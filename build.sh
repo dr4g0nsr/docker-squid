4c68a3c94e57114e8d1a26794a99e5f333edbefc
Dragutin Cirkovic <dragonmen@gmail.com>
2021-10-02 22:44:48 +0200

    New stuff

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build . -t dr4g0nsr/docker-squid -t dr4g0nsr/docker-squid:latest
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+SQUID_EXEC="squid"
+
 create_log_dir() {
   mkdir -p ${SQUID_LOG_DIR}
   chmod -R 755 ${SQUID_LOG_DIR}
@@ -12,17 +14,22 @@ create_cache_dir() {
   chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
 }
 
+if [[ -z ${CACHE_MANAGER} ]]; then
+  echo "Enabling cache manager"
+  nginx
+fi
+
+if [[ -z ${ELITE_PROXY} ]]; then
+  echo "Elite proxy settings"
+fi
+
+if [[ -z ${TRAFFIC_LIMIT} ]]; then
+  echo "Traffic limit set for SQUID (trickle)"
+  SQUID_EXEC="trickle -d 1000 -u 1000 squid"
+fi
+
 create_log_dir
 create_cache_dir
-
-# allow arguments to be passed to squid
-if [[ ${1:0:1} = '-' ]]; then
-  EXTRA_ARGS="$@"
-  set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
-  EXTRA_ARGS="${@:2}"
-  set --
-fi
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
@@ -31,7 +38,9 @@ if [[ -z ${1} ]]; then
     $(which squid) -N -f /etc/squid/squid.conf -z
   fi
   echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec ${SQUID_EXEC} -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  read -p "Press enter to continue"
 else
   exec "$@"
+  read -p "Press enter to continue"
 fi
