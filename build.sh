1cadfe96ff7969e37476c122c8d5d3423b3464f0
Dragutin Cirkovic <dragonmen@gmail.com>
2021-10-03 16:28:11 +0200

    Sh for handling params

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . -t dr4g0nsr/docker-squid -t dr4g0nsr/docker-squid:latest
+docker build . -t dr4g0nsr/docker-squid -t dr4g0nsr/docker-squid:latest -t dr4g0nsr/docker-squid:1.0
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,18 +14,42 @@ create_cache_dir() {
   chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
 }
 
-if [[ -z ${CACHE_MANAGER} ]]; then
-  echo "Enabling cache manager"
-  nginx
+if [[ ! -z ${CACHE_MANAGER} ]]; then
+  echo "Enabling cache manager: starting nginx and fcgiwrap"
+  nginx > /dev/null 2>&1
+  /etc/init.d/fcgiwrap start > /dev/null 2>&1
 fi
 
-if [[ -z ${ELITE_PROXY} ]]; then
+if [[ ! -z SQUID_LIMIT ]]; then
+  echo "Set squid limit (pool)"
+  SQUID_PARTS=($SQUID_DOWNLOAD_UPLOAD)
+  #echo "Traffic limit set for SQUID (trickle) DL ${SQUID_PARTS[0]} KB/sec / UL ${SQUID_PARTS[1]} KB/sec"
+  cp /etc/squid/squid.conf /etc/squid/squid.conf.backup
+  sed -i "s=#include /etc/squid/squid-delaypools.conf=include /etc/squid/squid-delaypools.conf=g" /etc/squid/squid.conf.backup
+  cat /etc/squid/squid.conf.backup > /etc/squid/squid.conf
+else
+  cp /etc/squid/squid.conf /etc/squid/squid.conf.backup
+  sed -i "s=include /etc/squid/squid-delaypools.conf=#include /etc/squid/squid-delaypools.conf=g" /etc/squid/squid.conf.backup
+  cat /etc/squid/squid.conf.backup > /etc/squid/squid.conf
+fi
+
+if [[ ! -z ${ELITE_PROXY} ]]; then
   echo "Elite proxy settings"
+  cp /etc/squid/squid.conf /etc/squid/squid.conf.backup
+  sed -i "s=include /etc/squid/squid-anonymous.conf=#include /etc/squid/squid-anonymous.conf=g" /etc/squid/squid.conf.backup
+  cat /etc/squid/squid.conf.backup > /etc/squid/squid.conf
+else
+  cp /etc/squid/squid.conf /etc/squid/squid.conf.backup
+  sed -i "s=include /etc/squid/squid-anonymous.conf=#include /etc/squid/squid-anonymous.conf=g" /etc/squid/squid.conf.backup
+  cat /etc/squid/squid.conf.backup > /etc/squid/squid.conf
 fi
 
-if [[ -z ${TRAFFIC_LIMIT} ]]; then
-  echo "Traffic limit set for SQUID (trickle)"
-  SQUID_EXEC="trickle -d 1000 -u 1000 squid"
+if [[ ! -z ${TRAFFIC_LIMIT} ]]; then
+  TRAFFIC_PARTS=($TRAFFIC_DOWNLOAD_UPLOAD)
+  echo "Traffic limit set for SQUID (trickle) DL ${TRAFFIC_PARTS[0]} KB/sec / UL ${TRAFFIC_PARTS[1]} KB/sec"
+  SQUID_EXEC="trickle -d ${TRAFFIC_PARTS[0]} -u ${TRAFFIC_PARTS[1]} squid"
+else
+  SQUID_EXEC="squid"
 fi
 
 create_log_dir
@@ -38,7 +62,7 @@ if [[ -z ${1} ]]; then
     $(which squid) -N -f /etc/squid/squid.conf -z
   fi
   echo "Starting squid..."
-  exec ${SQUID_EXEC} -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec ${SQUID_EXEC} -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS} > /dev/null 2>&1
   read -p "Press enter to continue"
 else
   exec "$@"
